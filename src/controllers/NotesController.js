9d874efea2b92b6324d863f459045a591fccbaa4
ricardo <ricardo_fabbro@hotmail.com>
2023-05-30 22:09:55 -0300

    Criado Index das Notas

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,3 +1,4 @@
+const { request, response } = require("express");
 const knex = require("../database/knex");
 
 class NotesController {
@@ -49,6 +50,36 @@ class NotesController {
             links
         });
     }
+
+    async delete(request, response) {
+        const { id } = request.params;
+
+        await knex("notes").where({ id }).delete();
+
+        return response.json();
+    }
+
+    async index(request, response) {
+        const { title, user_id, tags } = request.query;
+
+        let notes;
+
+        if(tags) {
+            const filterTags = tags.split(',').map(tag => tag.trim());
+
+            notes = await knex("tags")
+                .whereIn("name", filterTags)
+
+        } else {
+          notes = await knex("notes")
+            .where({ user_id })
+            .whereLike("title", `%${title}%`)
+            .orderBy("title");       
+        }
+
+        return response.json(notes);
+    }
+
 }
 
 module.exports = NotesController;
